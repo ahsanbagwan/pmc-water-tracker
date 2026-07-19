d1313ab4f29eadfa9538c8678f74ec848e9f0dba
ahsan <ahsanbagwan@gmail.com>
2026-07-19 23:33:52 +0530

    Change cors config

--- backend/src/main/java/org/punewatertracker/config/WebConfig.java
+++ backend/src/main/java/org/punewatertracker/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:*")
+                .allowedOriginPatterns("http://localhost:*", "https://pmc-water-tracker.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
