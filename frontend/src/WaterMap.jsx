bd9bba56bac80db230b608be815720a50f6428f9
ahsan <ahsanbagwan@gmail.com>
2026-08-09 10:41:08 +0530

    change map

--- frontend/src/WaterMap.jsx
+++ frontend/src/WaterMap.jsx
@@ -2,6 +2,7 @@ import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet'
 import { STATUS_CONFIG } from './statusConfig'
 import WaterHeatmap from './WaterHeatmap.jsx'
 import WardBoundaries from './WardBoundaries.jsx'
+import 'leaflet.heat/dist/leaflet-heat.js'
 
 const PUNE_CENTER = [18.5204, 73.8567]
 
